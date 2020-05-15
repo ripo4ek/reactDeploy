474433b288ab9f88b7a4e0e48849306771417ae4
Immortal <Lyap-andr@yandex.ru>
2020-05-15 13:21:37 +0300

    Updates

---
+++ precache-manifest.72eb65123a090003ef397ad830b13dbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230b648be1c14a28604013116a9f5b74",
+    "url": "/reactDeploy/index.html"
+  },
+  {
+    "revision": "a5f3fb651de56cd795c2",
+    "url": "/reactDeploy/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "dfa5273f0477ca8b135c",
+    "url": "/reactDeploy/static/css/main.02185ea8.chunk.css"
+  },
+  {
+    "revision": "a5f3fb651de56cd795c2",
+    "url": "/reactDeploy/static/js/2.93e5e640.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/reactDeploy/static/js/2.93e5e640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa5273f0477ca8b135c",
+    "url": "/reactDeploy/static/js/main.7bb0da53.chunk.js"
+  },
+  {
+    "revision": "4575b7feb888fa10dcd2",
+    "url": "/reactDeploy/static/js/runtime-main.2a5e7551.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactDeploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.b02bb511697493e306685597a0d26c83.js"
+  "/reactDeploy/precache-manifest.72eb65123a090003ef397ad830b13dbf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactDeploy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
